b4ab75d485dff3b48a4e85143edfc976a45ccb0e
Alabi Emmanuel <bobslegend795@gmail.com>
2017-08-15 12:21:15 +0100

    Completed calculator challenge

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,52 @@
 #write your code here
+
+# ADD
+def add(a,b)
+    a + b
+end
+
+# SUBTRACT
+def subtract(a,b)
+    a - b
+end
+
+# SUM
+def sum(a)
+    result = 0;
+    if a.length == 0
+        return result
+    else
+        a.each do |n|
+            result += n
+        end
+    end
+
+    return result
+end
+
+# MULTIPLY
+def multiply a
+    result = 1
+    a.each do |n|
+        result *= n
+    end
+
+    return result
+end
+
+# POWER
+def power(a,b)
+    a**b
+end
+
+# FACTORIAL
+def factorial a
+    if a == 0 || a == 1
+        1
+    else
+        a * factorial(a-1)
+    end
+    
+end
+
+puts factorial(10)
