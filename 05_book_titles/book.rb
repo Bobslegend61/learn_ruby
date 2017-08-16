b487ea8f8cac54c20593e08262f621e02ec1f8e2
Alabi Emmanuel <bobslegend795@gmail.com>
2017-08-16 19:26:12 +0100

    completed book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,15 @@
 class Book
 # write your code here
+  attr_accessor :title
+
+  def title=(book)
+    exp = ["a","and","the","to","is","an","there","in","for","at","on","of"]
+    @title = book.split(" ").map.with_index do |w, i|
+      if !exp.include?(w) || (i == 0 && exp.include?(w))
+        w.capitalize
+      else
+        w
+      end
+    end.join(" ")
+  end
 end
