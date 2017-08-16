1e8c82cd17fad5b74026c7f50a3eaace4f5d1ead
Alabi Emmanuel <bobslegend795@gmail.com>
2017-08-16 08:04:20 +0100

    simon_says completed

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,45 @@
 #write your code here
+
+# SIMON SAYS
+
+# ECHO HELLO
+def echo greeting
+    greeting
+end
+
+# SHOUT
+def shout what
+    what.upcase
+end
+
+# REPEAT
+def repeat (what, how_many = 2)
+  ("#{what} " * how_many).strip
+end
+
+# START OF WORD
+def start_of_word(word, how_many)
+    word[0..how_many - 1]
+end
+
+# FIRST WORD
+def first_word word
+    word.split(" ")[0]
+end
+
+# TITLEIZE
+def titleize(word)
+    words_no_cap = ["and", "or", "the", "over", "to", "the", "a", "but"];
+    result = ""
+    word.split(" ").each_with_index do |arg, i|
+        if words_no_cap.include?(arg) && i != 0
+          result += " "+arg
+        else
+          result += " "+arg.capitalize
+        end
+    end
+
+    return result.strip
+end
+
+puts titleize("hello boy")
